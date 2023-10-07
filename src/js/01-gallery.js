41b5247692a884db83b9fcfa17f5537bc40f19c1
Oksana <oksanaparakonnaa32@gmail.com>
2023-10-07 19:33:13 +0300

    made task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItemLayout = galleryItem =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+  <img
+  class="gallery__image"
+  src="${galleryItem.preview}"
+  data-source="${galleryItem.original}"
+  alt="${galleryItem.description}"
+  />
+  </a>
+  </li>
+  `;
+
+(() => {
+  const items = galleryItems.map(item => galleryItemLayout(item)).join('');
+  gallery.insertAdjacentHTML('afterbegin', items);
+})();
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
