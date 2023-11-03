c1238db1251e9a028e323bcdbbac568467ac6246
Oksana <oksanaparakonnaa32@gmail.com>
2023-11-03 20:11:51 +0200

    last commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,23 +7,11 @@ const LOCAL_KEY = 'videoplayer-current-time';
 player.on(
   'timeupdate',
   throttle(function ({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', seconds);
+    localStorage.setItem(LOCAL_KEY, seconds);
     console.log('localStorage');
   }, 1000)
 );
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+const savedTime = localStorage.getItem(LOCAL_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,15 +5,18 @@ const refs = {
   textarea: document.querySelector('[name= "message"]'),
 };
 const LOCAL_KEY = 'feedback-form-state';
+
 refs.form.addEventListener('input', throttle(onInput, 500));
+refs.textarea.addEventListener('submit', onSubmit);
+
+let data = JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? {};
+
 function onInput(event) {
-  const data = JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? {};
   data[event.target.name] = event.target.value;
   localStorage.setItem(LOCAL_KEY, JSON.stringify(data));
-  console.log(data);
 }
-refs.textarea.addEventListener('sabmit', onSabmit);
-function onSabmit(event) {
+
+function onSubmit(event) {
   event.preventDefault();
   if (!email.value || !textarea.value) {
     alert('Будь ласка, заповніть усі поля!');
@@ -22,8 +25,10 @@ function onSabmit(event) {
         ${textarea.value}`);
     localStorage.removeItem(LOCAL_KEY);
     event.currentTarget.reset();
+    data = {};
   }
 }
+
 window.addEventListener('load', onload);
 function onload() {
   const dataOn = JSON.parse(localStorage.getItem(LOCAL_KEY));
