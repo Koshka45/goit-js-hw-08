9f9f4a0aae1c17fbcff741c3c6bd09dadeb25a0d
Oksana <oksanaparakonnaa32@gmail.com>
2023-11-04 22:35:23 +0200

    last commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,7 @@ const refs = {
 const LOCAL_KEY = 'feedback-form-state';
 
 refs.form.addEventListener('input', throttle(onInput, 500));
-refs.textarea.addEventListener('submit', onSubmit);
+refs.form.addEventListener('submit', onSubmit);
 
 let data = JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? {};
 
@@ -22,17 +22,15 @@ function onSubmit(event) {
     alert('Будь ласка, заповніть усі поля!');
   } else {
     console.log(`email: ${email.value} message:
-        ${textarea.value}`);
+          ${textarea.value}`);
     localStorage.removeItem(LOCAL_KEY);
     event.currentTarget.reset();
     data = {};
   }
 }
-
 window.addEventListener('load', onload);
 function onload() {
   const dataOn = JSON.parse(localStorage.getItem(LOCAL_KEY));
-  console.log(dataOn);
   if (dataOn) {
     refs.textarea.value = dataOn.message || '';
     refs.email.value = dataOn.email || '';
